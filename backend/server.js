b7db75bfe746ebe83e42b21f00ee5e5d022f29be
François-Xavier Savary <fx.savary@orange.fr>
2023-09-16 11:32:11 +0200

    server node en place connexion à BDD etablie, recup des données OK

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,46 @@
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// importe les fichiers de routes et les utilise
-const productRoutes = require('./routes/productRoutes.js');
+// connexion à la base de données
+const connection = mysql.createConnection({
+    host: 'localhost', // adresse du serveur MySQL
+    user: 'root', // nom utilisateur MySQL
+    password: 'FXMysql2023!', // mot de passe MySQL
+    database: 'productscatalogue' // nom BDD MySQL
+});
 
-// utilise les routes
-app.use('/api/products', productRoutes);
+connection.connect((err) => {
+    if(err) {
+        console.error('Erreur lors de la recupération des datas produits: '+ err.stack)
+        return;
+    } console.log("connexion reussie avec la BDD")
+});
 
+app.get("/categories", (req, res) => {
+    connection.query('SELECT * FROM categories',  (err, results) => {
+        if(err) {
+            console.error('Erreur lors de la recupération des datas produits: ', err)
+            res.status(500).json({ error: 'Erreur lors de la recupération des datas produits'});
+            return; 
+        }
+        // renvoie les resultats au format JSON en réponse
+        res.json(results);
+    });
+});
+
+app.get("/products", (req, res) => {
+    connection.query('SELECT * FROM products',  (err, results) => {
+        if(err) {
+            console.error('Erreur lors de la recupération des datas produits: ', err)
+            res.status(500).json({ error: 'Erreur lors de la recupération des datas produits'});
+            return; 
+        }
+        // renvoie les resultats au format JSON en réponse
+        res.json(results);
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Serveur backend en cours d'execution sur le port ${PORT}`);

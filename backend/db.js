bdd9f933805f6a1cff1fb4b86295be82c9a30f69
Efyx-07 <fx.savary@orange.fr>
2023-09-20 10:33:49 +0200

    Backend: Renommage de connection BDD catalogueproducts en vue de la création de la BDD users

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2'); // appelle l'instance mysql2
 
 // connexion à la base de données
-const connection = mysql.createConnection({
+const productsCatalogueConnection = mysql.createConnection({
     host: 'localhost', // adresse du serveur MySQL
     user: 'root', // nom utilisateur MySQL
     password: 'FXMysql', // mot de passe MySQL
@@ -9,11 +9,11 @@ const connection = mysql.createConnection({
 });
 
 // vérifie que la connexion est établie, envoie une erreur si echec 
-connection.connect((err) => {
+productsCatalogueConnection.connect((err) => {
     if(err) {
         console.error('Erreur lors de la recupération des datas produits: '+ err.stack)
         return;
     } console.log("connexion reussie avec la BDD")
 });
 
-module.exports = connection;
+module.exports = productsCatalogueConnection;

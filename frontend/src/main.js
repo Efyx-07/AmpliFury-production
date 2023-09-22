c54cec3ad5726b8b19a09cfa265b373f88635735
Efyx-07 <fx.savary@orange.fr>
2023-09-22 15:21:12 +0200

    persistence du token après rafraichissement OK

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import { useCatalogueStore } from './stores/CatalogueStore';
+import { useUserStore } from './stores/UserStore';
 
 import App from './App.vue';
 import router from './router';
+
 
 // Attend la fin du chargement des données pour initialiser l'application
 const initApp = async () => {
@@ -20,6 +22,16 @@ const initApp = async () => {
   } catch (error) {
     console.error('Erreur lors du chargement des données :', error);
   }
+
+  // recupère le token dans le localStorage, permet à l'utilisateur de rester connecté après rafraichissemnt de la page
+  const userStore = useUserStore();
+  const storedToken = localStorage.getItem('token');
+
+  if (storedToken) {
+    // si un token est present dans le localStorage, le stocke dans le store Pinia
+    userStore.setToken(storedToken);
+    userStore.isConnected = true;
+  };
 
   app.use(router);
   app.mount('#app');

6f14887f00419d6d200fccddba2780a671d89710
François-Xavier Savary <fx.savary@orange.fr>
2024-01-09 10:41:18 +0100

    update components

--- frontend/src/sub-components/Logo.vue
+++ frontend/src/sub-components/Logo.vue
@@ -1,58 +1,50 @@
 <template>
     <router-link to="/" @click="handleNavButtonClick">
-        <img :src="storeLogo.image.source" :alt="storeLogo.image.alt" :class="logoClasses">
-    </router-link>
-    
+        <img src="/src/assets/logo/AmpliFury.png" alt="AmpliFury logo" :class="logoClasses">
+    </router-link>   
 </template>
 
 <script setup>
 
-    import { computed } from 'vue';
+import { computed } from 'vue';
 
-    // datas
-    const storeLogo = {
-        image: {
-            source: '/src/assets/logo/AmpliFury.png',
-            alt: 'AmpliFury logo'
-        }
-    }
+// recupération des props définies dans les composants parents
+const props = defineProps({
+    parentComponent: String,
+    closeBurgerMenu: Function
+});
 
-    // recupération des props définies dans les composants parents
-    const props = defineProps({
-        parentComponent: String,
-        closeBurgerMenu: Function
-    });
+// gestion des classes conditionnelles
+const logoClasses = computed(() => ({
+    'logo-header': props.parentComponent === 'header',
+    'logo-burgermenu': props.parentComponent === 'burgermenu'
+}));
 
-    // gestion des classes conditionnelles
-    const logoClasses = computed(() => ({
-        'logo-header': props.parentComponent === 'header',
-        'logo-burgermenu': props.parentComponent === 'burgermenu'
-    }));
-
-    // récupération via props de la fonction closeBurgerMenu definie dans composant parent BurgerMenu
-    const handleNavButtonClick = () => {
-        if(props.parentComponent === 'burgermenu') { // n'active la fonction que quand le composant parent est BurgerMenu
-            props.closeBurgerMenu();
-        };
+// récupération via props de la fonction closeBurgerMenu definie dans composant parent BurgerMenu
+const handleNavButtonClick = () => {
+    if(props.parentComponent === 'burgermenu') { // n'active la fonction que quand le composant parent est BurgerMenu
+        props.closeBurgerMenu();
     };
+};
 
 </script>
 
 <style lang="scss" scoped>
-    @import '@/assets/sass/variables.scss'; 
-    .logo-header {
-        width: 2.5rem;
 
-        @media screen and (min-width: $breakpointTablet) {
-            width: 4.5rem;
-        }
+@import '@/assets/sass/variables.scss'; 
+.logo-header {
+    width: 2.5rem;
 
-        @media screen and (min-width: $breakpointDesktop) {
-            width: 6rem;
-        }
+    @media screen and (min-width: $breakpointTablet) {
+        width: 4.5rem;
     }
 
-    .logo-burgermenu {
-        width: 5rem;
+    @media screen and (min-width: $breakpointDesktop) {
+        width: 6rem;
     }
+}
+.logo-burgermenu {
+    width: 5rem;
+}
+
 </style>
